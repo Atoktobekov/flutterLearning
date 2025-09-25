8e5c06ac9fd5beedd9e0b8ab4d26d622e15e6ae4
Adi <atoktobekov1@gmail.com>
2025-09-25 13:07:06 +0600

    move textTheme to separate file

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final textTheme = TextTheme(
+  bodyMedium: const TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  ),
+
+  titleSmall: TextStyle(
+    color: Colors.white.withOpacity(.7),
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  ),
+);
