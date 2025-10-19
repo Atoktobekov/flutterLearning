cccdc61ceac497f376f03c997733b79c7f05d9b5
Adi <atoktobekov1@gmail.com>
2025-10-19 13:01:21 +0600

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,12 @@ import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final talker = TalkerFlutter.init();
   Bloc.observer = TalkerBlocObserver(
     talker: talker,
@@ -23,12 +27,19 @@ void main() {
   GetIt.instance<Talker>().debug("Talker started!");
 
   final dio = Dio();
+
   dio.interceptors.add(
     TalkerDioLogger(
       settings: const TalkerDioLoggerSettings(printResponseData: false),
       talker: talker,
     ),
   );
+
+  final app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  talker.info(app.options.projectId);
 
   GetIt.instance.registerLazySingleton<IfCryptoCoinsRepository>(
     () => CryptoCoinsRepository(dio: dio),
