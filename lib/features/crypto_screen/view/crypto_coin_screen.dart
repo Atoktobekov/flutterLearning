1abb1ebcb620bdece1702439265e282e0e8444d6
Adi <atoktobekov1@gmail.com>
2025-10-22 20:30:25 +0600

    update crypto_coin_screen.dart
    -use CachedNetworkImage instead of Image.network
    
    -show last update time based on coin data
    
    -delete old timer methods

--- lib/features/crypto_screen/view/crypto_coin_screen.dart
+++ lib/features/crypto_screen/view/crypto_coin_screen.dart
@@ -6,8 +6,9 @@ import 'package:get_it/get_it.dart';
 import 'package:learning/features/crypto_screen/bloc/crypto_coin_details/crypto_coin_details_bloc.dart';
 import 'package:learning/features/crypto_screen/view/widgets/widgets.dart';
 import 'package:learning/repositories/crypto_coins/crypto_coins.dart';
-
+import 'package:intl/intl.dart';
 import 'package:learning/theme/theme.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CryptoCoinScreen extends StatefulWidget {
   const CryptoCoinScreen({super.key});
@@ -21,9 +22,6 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
   final _coinDetailsBloc = CryptoCoinDetailsBloc(
     GetIt.I<IfCryptoCoinsRepository>(),
   );
-
-  Timer? _timer;
-  Duration _elapsed = Duration.zero;
 
   @override
   void didChangeDependencies() {
@@ -39,7 +37,6 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
 
   @override
   void dispose() {
-    _timer?.cancel();
     super.dispose();
   }
 
@@ -66,7 +63,9 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
           builder: (context, state) {
             if (state is CryptoCoinDetailsLoaded) {
               final coin = state.coin;
-              _startTimer(); // start after loading
+              final formattedDate = DateFormat(
+                'dd MMM yyyy, HH:mm:ss',
+              ).format(coin.details.lastUpdate);
 
               return SingleChildScrollView(
                 physics: const AlwaysScrollableScrollPhysics(),
@@ -81,7 +80,16 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
                     SizedBox(
                       height: 160,
                       width: 160,
-                      child: Image.network(coin.details.fullImageUrl),
+                      child: CachedNetworkImage(
+                        imageUrl: coin.details.fullImageUrl,
+                        placeholder: (context, url) =>
+                            const Center(child: CircularProgressIndicator()),
+                        errorWidget: (context, url, error) => Image.asset(
+                          'assets/images/placeholder.png',
+                          height: 160,
+                          width: 160,
+                        ),
+                      ),
                     ),
                     const SizedBox(height: 24),
                     Text(
@@ -108,25 +116,23 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
                         children: [
                           _DataRow(
                             title: 'High 24 Hour',
-                            value: '${roundTo(coin.name, coin.details.high24Hours)} \$',
+                            value:
+                                '${roundTo(coin.name, coin.details.high24Hours)} \$',
                           ),
                           const SizedBox(height: 10),
                           _DataRow(
                             title: 'Low 24 Hour',
-                            value: '${roundTo(coin.name, coin.details.low24Hours)} \$',
+                            value:
+                                '${roundTo(coin.name, coin.details.low24Hours)} \$',
                           ),
                           const SizedBox(height: 10),
                           _DataRow(
                             title: 'Change 24 Hour',
-                            value: '${roundTo(coin.name, coin.details.change24Hours)} \$',
+                            value:
+                                '${roundTo(coin.name, coin.details.change24Hours)} \$',
                           ),
                           const SizedBox(height: 10),
-                          _DataRow(
-                            title: 'Last update',
-                            value: _coinDetailsBloc.updateTime == null
-                                ? '...'
-                                : _formatElapsed(_elapsed),
-                          ),
+                          _DataRow(title: 'Last update', value: formattedDate),
                         ],
                       ),
                     ),
@@ -168,18 +174,6 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
       ),
     );
   }
-
-  void _startTimer() {
-    _timer?.cancel();
-    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
-      final updateTime = _coinDetailsBloc.updateTime;
-      if (updateTime != null && mounted) {
-        setState(() {
-          _elapsed = DateTime.now().difference(updateTime);
-        });
-      }
-    });
-  }
 }
 
 class _DataRow extends StatelessWidget {
@@ -202,20 +196,8 @@ class _DataRow extends StatelessWidget {
   }
 }
 
-String _formatElapsed(Duration elapsed) {
-  if (elapsed.inSeconds < 60) {
-    return '${elapsed.inSeconds} seconds ago';
-  } else if (elapsed.inMinutes < 60) {
-    return '${elapsed.inMinutes} minutes ago';
-  } else if (elapsed.inHours < 24) {
-    return '${elapsed.inHours} hours ago';
-  } else {
-    return '${elapsed.inDays} days ago';
-  }
-}
-
 double roundTo(String name, double value) {
-  if(name == "AID" || name == "CAG" || name == "DOV"){
+  if (name == "AID" || name == "CAG" || name == "DOV") {
     return value;
   }
   num mod = pow(10.0, 5);
